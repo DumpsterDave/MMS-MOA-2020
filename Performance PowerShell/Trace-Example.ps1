a15395177d66072c86bffe632a1bdc0960441738
DumpsterDave <35818070+DumpsterDave@users.noreply.github.com>
2021-09-12 09:58:02 -0500

    Updated

--- Performance PowerShell/Trace-Example.ps1
+++ Performance PowerShell/Trace-Example.ps1
@@ -1,22 +1,33 @@
-#region setup and get baseline
-Import-Module "c:\Users\scott.corio\Documents\GitHub\MMS-MOA-2020\Performance PowerShell\PSPerformance.psm1"
+#region setup
+Import-Module "$(Get-Location)\PSPerformance.psm1"
 
-$Baseline = Test-Performance -Count 10 -ScriptBlock {Get-Service | Where-Object {$_.Status -eq 'Running'}}
-$Baseline
+$Services = Get-Service
 #endregion
 
 #region Initial Trace-Command
-Trace-Command -Name TypeConversion -ListenerOption Timestamp -Expression {Get-Service | Where-Object {$_.Status -eq 'Running'}} -PSHost
+$BaselineCode = {
+    foreach ($svc in $Services) {
+        if ($svc.Status -eq 'Running' -and $Svc.StartupType -eq 'Automatic') {
+            Write-Output $svc.Name
+        }
+    }
+}
+Trace-Command -Name TypeConversion -ListenerOption Timestamp -Expression $BaselineCode -PSHost
 #endregion
 
 #region Trace-Command with Enum Called out
-$Running = [System.ServiceProcess.ServiceControllerStatus]::Running
-Trace-Command -Name TypeConversion -ListenerOption Timestamp -Expression {Get-Service | Where-Object {$_.Status -eq $Running}} -PSHost
+$OptimizedCode = {
+    foreach ($svc in $Services) {
+        if ($svc.Status -eq [System.ServiceProcess.ServiceControllerStatus]::Running -and $Svc.StartupType -eq [System.ServiceProcess.ServiceStartMode]::Automatic) {
+            Write-Output $svc.Name
+        }
+    }
+}
+Trace-Command -Name TypeConversion -ListenerOption Timestamp -Expression $OptimizedCode -PSHost
 #endregion
 
 #Lets Compare
-$Running = [System.ServiceProcess.ServiceControllerStatus]::Running
-$Baseline = Test-Performance -Count 10 -ScriptBlock {Get-Service | Where-Object {$_.Status -eq 'Running'}}
-$WithEnum = Test-Performance -Count 10 -ScriptBlock {Get-Service | Where-Object {$_.Status -eq $Running}}
+$Baseline = Test-Performance -Count 10 -ScriptBlock $BaselineCode
+$WithEnum = Test-Performance -Count 10 -ScriptBlock $OptimizedCode
 Get-Winner -AName 'Baseline' -AValue $Baseline.Median -BName "Enumerated" -BValue $WithEnum.Median
 #endregion
